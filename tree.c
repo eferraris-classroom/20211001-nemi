ddf904f4fa10b5acffed171f4676637416bf7963
bauti <jnemi@fie.undef.edu.ar>
2021-10-01 16:04:02 -0300

    ADD - struct root declaration

---
+++ tree.c
@@ -0,0 +1,5 @@
+//
+// Created by bauti on 10/1/2021.
+//
+
+#include "tree.h"
---
+++ tree.h
@@ -0,0 +1,19 @@
+//
+// Created by bauti on 10/1/2021.
+//
+
+#ifndef BASE_PROJECT_TREE_H
+#define BASE_PROJECT_TREE_H
+
+typedef struct root {
+    int value;
+    struct root * left;
+    struct root * right;
+} Root;
+
+Root* newRoot(int value);
+void insertInTree(Root** root, Root* n);
+
+void clearTree(Root** root);
+
+#endif //BASE_PROJECT_TREE_H
