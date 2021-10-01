98bcc1831d0010dd88af47c9924cf5c000d01eb0
bauti <jnemi@fie.undef.edu.ar>
2021-10-01 16:13:31 -0300

    ADD - data request for tree insertion

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include "tree.h"
+#define N 5
+
 
 int main() {
     printf("Hello, World!\n");
+    Root* root = NULL;
+    int i, number;
+    for (i = 0; i < N; i++) {
+        printf("Please, enter a number: ");
+        scanf("%d", &number);
+        insertInTree(&root, newRoot(number));
+    }
     return 0;
 }
